41b25421dcbf28bdfb6e08f775a5645c4dffe5e4
bruna <bruciarlo@yahoo.com.br>
2025-02-24 15:02:30 -0300

    ajustes de deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,22 +1,30 @@
 import express from 'express';
 import { db } from './connect.js';
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 const PORT = 3001;
+const __dirname = path.resolve();
 
 app.use(cors());
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send('Atualizar');
 })
 
-app.get('/artists', async(request, response) => {
+app.get('/api/artists', async(request, response) => {
     response.send(await db.collection('artists').find({}).toArray());
 })
 
-app.get('/songs', async(request, response) => {
+app.get('/api/songs', async(request, response) => {
     response.send(await db.collection('songs').find({}).toArray());
+})
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+
+app.get('*', async(request, response) => {
+    response.sendFile(path.join(__dirname, "../../front-end/dist/index.html"));
 })
 
 app.listen(PORT, () => {
